7143335c9afdf215ed0c33d73e95f97ed05cfc13
luanvmendes <luan_vmendes@hotmail.com>
2020-03-19 15:00:50 -0300

    Aulas 2.11 a 2.14

--- src/main/java/com/gft/cobranca/controller/TituloController.java
+++ src/main/java/com/gft/cobranca/controller/TituloController.java
@@ -5,10 +5,13 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.gft.cobranca.model.StatusTitulo;
 import com.gft.cobranca.model.Titulo;
@@ -24,18 +27,20 @@ public class TituloController {
 	@RequestMapping("/novo")
 	public ModelAndView novo() {
 		ModelAndView mv = new ModelAndView("CadastroTitulo");
-		mv.addObject("todosStatusTitulo", StatusTitulo.values());
+		mv.addObject(new Titulo());
 		return mv;
 	}
 	
 	@RequestMapping(method = RequestMethod.POST)
-	public ModelAndView salvar(Titulo titulo) {
-
+	public String salvar(@Validated Titulo titulo, Errors errors, RedirectAttributes attributes) {
+		if (errors.hasErrors()) {
+			return "CadastroTitulo";
+		}
+		
 		titulos.save(titulo);
 		
-		ModelAndView mv = new ModelAndView("CadastroTitulo");
-		mv.addObject("mensagem", "Título salvo com sucesso");
-		return mv;
+		attributes.addFlashAttribute("mensagem", "Título salvo com sucesso");
+		return "redirect:/titulos/novo";
 	}
 	
 	@RequestMapping
